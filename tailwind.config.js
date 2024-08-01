d10d034883f8a2beb1460600c1b6b255f475ca18
Xretic <95174481+unf6@users.noreply.github.com>
2024-08-01 15:52:27 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import { createPreset } from 'fumadocs-ui/tailwind-plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './content/**/*.{md,mdx}',
+    './mdx-components.{ts,tsx}',
+    './node_modules/fumadocs-ui/dist/**/*.js',
+  ],
+  presets: [createPreset()],
+  theme: {
+    extend: {
+      colors: {
+        "c-primary": "var(--primary)",
+        "c-secondary": "var(--secondary)"
+      }
+    }
+  }
+};
